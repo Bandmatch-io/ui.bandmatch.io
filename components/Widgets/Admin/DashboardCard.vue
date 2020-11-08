d77b33c882f02b4939054ee6cbf06c7d90da340b
Tom Croasdale <tecroasdale@gmail.com>
2020-11-08 20:32:29 +0000

    Added dashboard links

---
+++ components/Widgets/Admin/DashboardCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <nuxt-link :to="link" class="inline-block mx-auto">
+    <div class="clickable w-48 h-48 hover:shadow-outline hover:bg-gray-100 rounded border bg-white overflow-hidden shadow m-8">
+      <div class="shadow-inner text-center">
+        <slot />
+      </div>
+      <div class="px-6 py-4 border-t bg-primary-grad">
+        <div class="font-bold text-xl mb-2 text-center text-white">
+          {{ title }}
+        </div>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    link: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/styles.scss';
+</style>
--- pages/admin/dashboard.vue
+++ pages/admin/dashboard.vue
@@ -1,9 +1,36 @@
 <template>
-  <div />
+  <div class="flex-container flex flex-wrap bg-polka">
+    <DashboardCard title="Activity" link="/admin/stats">
+      <trending-up-icon size="8x" class="block mx-auto" />
+    </DashboardCard>
+    <DashboardCard title="Reports" link="/admin/reports">
+      <alert-octagon-icon size="8x" class="block mx-auto" />
+    </DashboardCard>
+    <DashboardCard title="Users" link="/admin/users">
+      <user-icon size="8x" class="block mx-auto" />
+    </DashboardCard>
+    <DashboardCard title="Locations" link="/admin/locations">
+      <map-icon size="8x" class="block mx-auto" />
+    </DashboardCard>
+    <DashboardCard title="Referrals" link="/admin/referrals">
+      <navigation-icon size="8x" class="block mx-auto" />
+    </DashboardCard>
+  </div>
 </template>
 
 <script>
-export default {}
+import { TrendingUpIcon, AlertOctagonIcon, UserIcon, MapIcon, NavigationIcon } from 'vue-feather-icons'
+import DashboardCard from '~/components/Widgets/Admin/DashboardCard'
+export default {
+  components: {
+    DashboardCard,
+    TrendingUpIcon,
+    AlertOctagonIcon,
+    UserIcon,
+    MapIcon,
+    NavigationIcon
+  }
+}
 </script>
 
 <style lang="scss" scoped>
