db1aaa23c1625d301399d0522e7520ad5d10d2de
Tom Croasdale <tecroasdale@gmail.com>
2020-11-15 11:09:42 +0000

    Added spinning

--- pages/profile/edit.vue
+++ pages/profile/edit.vue
@@ -7,7 +7,7 @@
             View public profile
           </ButtonPrimary>
           <ButtonTertiary :action="() => { postUserProfile(user) }" group-pos="last" class="col-span-1 inline-block w-full mx-0">
-            <check-icon v-if="isSaved" class="inline-block" /><loader-icon v-else class="inline-block" /> <span class="hidden md:inline">Saved</span>
+            <check-icon v-if="isSaved" class="inline-block" /><loader-icon v-else class="inline-block spin" /> <span class="hidden md:inline">Saved</span>
           </ButtonTertiary>
         </div>
 
@@ -267,4 +267,42 @@ export default {
 
 <style lang="scss" scoped>
 @import '~/assets/scss/styles.scss';
+
+.spin {
+  -webkit-animation: rotating 2s linear infinite;
+  -moz-animation: rotating 2s linear infinite;
+  -ms-animation: rotating 2s linear infinite;
+  -o-animation: rotating 2s linear infinite;
+  animation: rotating 2s linear infinite;
+}
+
+@-webkit-keyframes rotating {
+  from {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotating {
+  from {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
 </style>
