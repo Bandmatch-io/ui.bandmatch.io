ebfbb89b1939a8b400696794a38fa6136fc2a762
Tom Croasdale <tecroasdale@gmail.com>
2020-11-19 19:52:50 +0000

    Fixed appearance of DashboardCard

--- components/Widgets/Admin/DashboardCard.vue
+++ components/Widgets/Admin/DashboardCard.vue
@@ -1,12 +1,12 @@
 <template>
   <nuxt-link :to="link" class="inline-block mx-8 my-5">
     <div class="w-48 rounded border bg-white overflow-hidden shadow" :class="{'clickable hover:shadow-outline hover:bg-gray-100': link !== ''}">
-      <div class="px-6 py-2 border-t bg-primary-grad">
+      <div class="px-6 py-2 rounded-t bg-primary-grad">
         <div class="font-bold text-lg mb-2 text-center text-white">
           {{ title }}
         </div>
       </div>
-      <div class="shadow-inner text-center">
+      <div class="text-center">
         <slot />
       </div>
     </div>
