be50293f99a56a9df61e4d4fdc97b0ea7fd2f47e
Tom Croasdale <tecroasdale@gmail.com>
2020-11-22 10:31:00 +0000

    user search on enter press

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="flex-container bg-polka">
     <div class="w-3/4 mx-auto block border rounded shadow p-3 bg-white m-3">
-      <TextInput v-model="query" label="User or email regex" class="max-w-350 block mx-auto mb-1">
-        <users-icon class="block mx-auto" />
-      </TextInput>
-      <div v-if="state===states.loading" class="block max-w-350 mx-auto w-full md:w-1/4 h-24 my-3">
-        <LoaderAnim />
-      </div>
-      <ButtonPrimary v-else :action="performSearch" class="block w-full max-w-350 mx-auto my-3">
-        <search-icon class="block mx-auto" />
-      </ButtonPrimary>
+      <form @submit.prevent="performSearch">
+        <TextInput v-model="query" label="User or email regex" class="max-w-350 block mx-auto mb-1">
+          <users-icon class="block mx-auto" />
+        </TextInput>
+        <div v-if="state===states.loading" class="block max-w-350 mx-auto w-full md:w-1/4 h-24 my-3">
+          <LoaderAnim />this.colours =
+        </div>
+        <ButtonPrimary v-else :action="performSearch" class="block w-full max-w-350 mx-auto my-3">
+          <search-icon class="block mx-auto" />
+        </ButtonPrimary>
+      </form>
     </div>
 
     <div class="w-3/4 mx-auto block">
