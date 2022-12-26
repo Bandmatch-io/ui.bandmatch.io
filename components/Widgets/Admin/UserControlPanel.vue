14370aec6ad5172ac08808f33d890d2f18442b6e
Tom Croasdale <tecroasdale@gmail.com>
2022-12-26 19:04:12 +0000

    ui#379: Allow admin to clear audio

--- components/Widgets/Admin/UserControlPanel.vue
+++ components/Widgets/Admin/UserControlPanel.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
-    <div class="w-full grid grid-cols-2 my-0">
+    <div class="w-full grid grid-cols-3 my-0">
       <Button colour="complementary" :action="clearName" group-pos="first" class="my-1 col-span-1 inline-block mx-0">
         <delete-icon class="inline-block" /> Clear Name
       </Button>
-      <Button colour="complementary" :action="clearDesc" group-pos="last" class="my-1 col-span-1 inline-block mx-0">
+      <Button colour="complementary" :action="clearDesc" group-pos="mid" class="my-1 col-span-1 inline-block mx-0">
         <delete-icon class="inline-block" /> Clear Description
+      </Button>
+      <Button colour="complementary" :action="clearAudio" group-pos="last" class="my-1 col-span-1 inline-block mx-0">
+        <delete-icon class="inline-block" /> Clear Audio
       </Button>
     </div>
     <div class="w-full grid grid-cols-1 my-0">
       <ConfirmationInput :check-string="user.displayName" class="my-1 col-span-1 inline-block mx-0" @confirm="deleteUser">
         <trash-2-icon class="inline-block" /> Delete user
       </ConfirmationInput>
-      <ConfirmationInput v-show="user.admin" :check-string="user.displayName" class="my-1 col-span-1 inline-block mx-0" @confirm="demoteUser">
+      <ConfirmationInput v-show="user.admin" :checkString="user.displayName" class="my-1 col-span-1 inline-block mx-0" @confirm="demoteUser">
         <x-square-icon class="inline-block" /> Demote from admin
       </confirmationinput>
-      <ConfirmationInput v-show="!user.admin" colour="secondary" :check-string="user.displayName" class="my-1 col-span-1 inline-block mx-0" @confirm="promoteUser">
+      <ConfirmationInput v-show="!user.admin" colour="secondary" :checkString="user.displayName" class="my-1 col-span-1 inline-block mx-0" @confirm="promoteUser">
         <award-icon class="inline-block" /> Promote to admin
       </confirmationinput>
     </div>
@@ -52,6 +55,15 @@ export default {
         .then((res) => {
           if (res.data.success) {
             this.$store.commit('toasts/create', { title: 'Admin | User', message: `Cleared ${this.user.displayName}'s description` })
+            this.$emit('refresh')
+          }
+        })
+    },
+    clearAudio () {
+      this.$axios.delete(`/admin/users/audio?uid=${this.user._id}`)
+        .then((res) => {
+          if (res.data.success) {
+            this.$store.commit('toasts/create', { title: 'Admin | User', message: `Cleared ${this.user.displayName}'s audio preview` })
             this.$emit('refresh')
           }
         })
